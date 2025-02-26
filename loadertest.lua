bec68b6b5f72e5b7a6a7f081f4306e06271f54fd
fagatabez <oliwierpruchnik@gmail.com>
2025-02-26 14:28:17 +0100

    Update and rename loader.lua to loadertest.lua

--- loadertest.lua
+++ loadertest.lua
@@ -1,28 +1,30 @@
 local files = {
-    "auto clicker.lua",
+    "auto%20clicker.lua",
     "esp.lua",
-    "fast kill.lua",
-    "na afka telepoti.lua",
-    "tower of hell.lua",
-    "telefon symulacja.lua",
+    "fast%20kill.lua",
+    "na%20afka%20telepoti.lua",
+    "tower%20of%20hell.lua",
+    "telefon%20symulacja.lua",
     "fogrem.lua"
 }
 
+local baseUrl = "https://raw.githubusercontent.com/fagatabez/haksy-pe/main/"
 local loadedFiles = {} -- Przechowuje informacje o załadowanych plikach
 
 for _, file in ipairs(files) do
     if loadedFiles[file] then
         print("⏩ Plik " .. file .. " został już załadowany, pomijam.")
     else
-        local url = "https://raw.githubusercontent.com/fagatabez/haksy/main/" .. file
+        local url = baseUrl .. file
         print("⏳ Pobieranie pliku: " .. file .. " z URL: " .. url)
 
-        local success, response = pcall(function()
-            return game:HttpGet(url)
+        local response
+        local success, err = pcall(function()
+            response = game:HttpGet(url, true)
         end)
 
         if success and response and response ~= "" then
-            local executeSuccess, err = pcall(function()
+            local executeSuccess, executeErr = pcall(function()
                 loadstring(response)()
             end)
 
@@ -30,12 +32,12 @@ for _, file in ipairs(files) do
                 print("✅ " .. file .. " załadowany pomyślnie!")
                 loadedFiles[file] = true  -- Zapisuje, że plik został pobrany
             else
-                warn("❌ Błąd wykonania pliku: " .. file .. " | " .. tostring(err))
+                warn("❌ Błąd wykonania pliku: " .. file .. " | " .. tostring(executeErr))
             end
         else
-            warn("⚠️ Błąd pobierania pliku: " .. file)
+            warn("⚠️ Błąd pobierania pliku: " .. file .. " | " .. tostring(err))
         end
     end
 
-    task.wait(0.5) -- Opóźnienie
+    task.wait(0.5) -- Opóźnienie dla stabilności
 end
