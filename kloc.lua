61a431c5936541fb4bd32ceb5d68fccfa694bf64
fagatabez <oliwierpruchnik@gmail.com>
2025-04-01 21:02:23 +0200

    Create kloc.lua

---
+++ kloc.lua
@@ -0,0 +1,5 @@
+local part = Instance.new("Part")
+part.Size = Vector3.new(40, 40, 5)
+part.CFrame = CFrame.new(-5, 13.6055689, 135.815948, -4.37113883e-08, 0, 1, 0, 1, 0, -1, 0, -4.37113883e-08)
+part.Anchored = true
+part.Parent = game.Workspace
